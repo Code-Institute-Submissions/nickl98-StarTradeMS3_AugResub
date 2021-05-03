ebc7086d9ab28da8f0b6fc2de1d0b7cb7a6a3532
nickl98 <nicklodde98@gmail.com>
2021-05-03 15:38:47 +0000

    Succesfully added a new user with a password_hash protection

--- app.py
+++ app.py
@@ -29,6 +29,23 @@ def get_trades():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        #check if username is already exists in DB
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        #put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Sign Up was succesful")
     return render_template("register.html")
 
 
