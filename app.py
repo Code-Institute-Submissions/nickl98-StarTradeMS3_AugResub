98edf959344e70554ff79075c9a647cec669e5e4
nickl98 <nicklodde98@gmail.com>
2021-08-03 01:56:01 +0000

    Turned Debug on for a little bit

--- app.py
+++ app.py
@@ -258,4 +258,4 @@ def delete_console(console_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True)
